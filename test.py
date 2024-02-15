d6a914ac9ed60dad1ca69bb9c2fd651529b94a77
GhwFluffy <ghw@ghwfluffy.com>
2024-02-14 21:59:41 -0600

    Algorithm now always buys a spread and always holds high

--- test.py
+++ test.py
@@ -6,83 +6,59 @@ from coinbase.rest import rest_base, accounts, products, orders
 from utils.wallet import get_wallet, wallets_equal
 from utils.pricebook import get_market_price
 from utils.debug import print_json, get_now_string
-from utils.transactions import buy_btc, sell_btc
-
-# How big of a spread to play with
-SPREAD_SIZE = 100
+from utils.transactions import buy_btc, sell_btc, cancel_order, is_order_complete
 
 CREDENTIAL="secrets/coinbase_cloud_api_key.json"
 
 # REST API instance
 ctx = rest_base.RESTBase(key_file=CREDENTIAL)
 
-market_prices = []
+# Algorithm:
+# Place bid/ask at market price +/- 500
+# if sell BTC:
+#   cancel buy
+#   place new buy at -500
+#   place new sell at +500
+# if buy BTC:
+#   place new buy at -500
+SPREAD_SIZE = 500
+WAGER_BTC = 0.0016
+
 last_wallet = {}
+sell_order = None
+buy_order = None
+
 while True:
-    # Get wallet
-    wallet = get_wallet(ctx)
-    reload = False
-
     # Current BTC price
-    market_price = get_market_price(ctx)['bid']
+    try:
+        market_price = get_market_price(ctx)['bid']
+    except:
+        print("[{}] Failed to get market price.".format(get_now_string()))
+        time.sleep(60)
+        continue
 
-    # Get the average price over the last two hours
-    market_prices.append(market_price)
-    average_price = sum(market_prices) / len(market_prices)
+    # Sell order is complete: Cancel buy and place new sell
+    if is_order_complete(ctx, sell_order):
+        if is_order_complete(ctx, buy_order) or cancel_order(ctx, "BUY", buy_order):
+            sell_order = sell_btc(ctx, WAGER_BTC, market_price + SPREAD_SIZE)
+            print("[{}] Sell order ID: {}".format(get_now_string(), sell_order))
 
-    # Keep 2 hours worth of data
-    while len(market_prices) > 120:
-        market_prices.pop(0)
+    # Buy order is complete: Place new buy order
+    if is_order_complete(ctx, buy_order):
+        buy_order = buy_btc(ctx, WAGER_BTC, market_price - SPREAD_SIZE)
+        print("[{}] Buy order ID: {}".format(get_now_string(), buy_order))
 
-    # We have money to spend
-    if wallet['USD']['available'] > 10:
-        # Buy below average value and current market value
-        buy_price = average_price - SPREAD_SIZE
-        if buy_price > market_price - SPREAD_SIZE:
-            buy_price = market_price - SPREAD_SIZE
-        buy_btc(ctx, wallet['USD']['available'] - 5, buy_price)
-        reload = True
-
-    # We have bitcoin to sell
-    if wallet['BTC']['available'] > 0.0001:
-        # Sell above average value and current market value
-        sell_price = average_price + SPREAD_SIZE
-        if sell_price < market_price + SPREAD_SIZE:
-            sell_price = market_price + SPREAD_SIZE
-        sell_btc(ctx, wallet['BTC']['available'], sell_price)
-        reload = True
-
-    # Cancel bids to buy bitcoin that are older than 2 hours
-    cur_orders = orders.list_orders(ctx, order_status=['OPEN'])
-    for order in cur_orders['orders']:
-        if order['side'] != "BUY":
-            continue
-        created = datetime.strptime(order['created_time'], "%Y-%m-%dT%H:%M:%S.%fZ").replace(tzinfo=timezone.utc)
-        now = datetime.now(timezone.utc)
-        time_difference = now - created
-        if time_difference > timedelta(hours=2):
-            result = orders.cancel_orders(ctx, order_ids=[order['order_id']])
-            success = False
-            try:
-                success = result['results'][0]['success']
-            except:
-                pass
-            print("[{}] [CANCEL] ".format(get_now_string()), end='')
-            if not success:
-                print("[ERROR] ", end='')
-            print("Order {} of {:.5f} BTC at ${:.2f}.".format(
-                order['client_order_id'],
-                float(order['order_configuration']['limit_limit_gtc']['base_size']),
-                float(order['order_configuration']['limit_limit_gtc']['limit_price'])))
-            reload = True
-
-    # Show updated wallet info
-    if reload:
+    # Print wallet changes
+    try:
         wallet = get_wallet(ctx)
-    if not wallets_equal(last_wallet, wallet):
-        print("[{}] Wallet:".format(get_now_string()))
-        print_json(wallet)
-        last_wallet = wallet
+        if not wallets_equal(last_wallet, wallet):
+            print("[{}] Wallet:".format(get_now_string()))
+            print_json(wallet)
+            last_wallet = wallet
+    except:
+        print("[{}] Error loading wallet.".format(get_now_string()))
+        time.sleep(60)
+        continue
 
     # Sleep 1 minute
-    time.sleep(60)
+    time.sleep(6)
--- utils/transactions.py
+++ utils/transactions.py
@@ -16,27 +16,37 @@ def floor(x, decimals):
 def gen_client_order_id(order_type):
     return str(int(time.time() * 1000)) + "-" + order_type
 
-def buy_btc(ctx, usd_quantity, bid_price):
+def buy_btc(ctx, amount, bid_price):
     client_order_id = gen_client_order_id('buy')
 
     bid_price = floor(bid_price, 2)
-    usd_quantity = floor(usd_quantity, 2)
-    amount = floor(usd_quantity / bid_price, 5)
+    usd_quantity = floor(amount * bid_price, 2)
 
-    #order_info = fake_order(
-    order_info = orders.limit_order_gtc_buy(
-        ctx,
-        client_order_id=client_order_id,
-        product_id='BTC-USD',
-        base_size=str(amount), # Amount of BTC to buy
-        limit_price=str(bid_price), # Don't buy anything more expensive than this (USD) (Higher than stop price)
-    )
+    try:
+        #order_info = fake_order(
+        order_info = orders.stop_limit_order_gtc_buy(
+            ctx,
+            client_order_id=client_order_id,
+            product_id='BTC-USD',
+            base_size=str(amount), # Amount of BTC to buy
+            stop_price=str(bid_price - 10), # Price to trigger at (USD)
+            limit_price=str(bid_price), # Don't buy anything more expensive than this (USD) (Higher than stop price)
+            stop_direction='STOP_DIRECTION_STOP_DOWN', # Trigger when below stop_price
+        )
+    except Exception as e:
+        print(e)
+        order_info = {'success': False}
 
+    order_id = None
     print("[{}] [BUY]  ".format(get_now_string()), end='')
     if not 'success' in order_info or not order_info['success']:
-        print("[ERROR] ", end='')
+        print("[ERROR] [{}] ".format(order_info['error_response']['error']), end='')
+    else:
+        order_id = order_info['order_id']
 
     print("{:.5f} bitcoin for ${:.2f} (${:.2f} BTC).".format(amount, usd_quantity, bid_price))
+
+    return order_id
 
 def sell_btc(ctx, btc_quantity, ask_price):
     client_order_id = gen_client_order_id('sell')
@@ -44,19 +54,64 @@ def sell_btc(ctx, btc_quantity, ask_price):
     ask_price = floor(ask_price, 2)
     btc_quantity = floor(btc_quantity, 5)
 
-    #order_info = fake_order(
-    order_info = orders.limit_order_gtc_sell(
-        ctx,
-        client_order_id=client_order_id,
-        product_id='BTC-USD',
-        base_size=str(btc_quantity), # Amount of BTC to sell
-        limit_price=str(ask_price), # Don't sell anything cheaper than this (USD) (Lower than stop price)
-    )
+    try:
+        #order_info = fake_order(
+        order_info = orders.stop_limit_order_gtc_sell(
+            ctx,
+            client_order_id=client_order_id,
+            product_id='BTC-USD',
+            base_size=str(btc_quantity), # Amount of BTC to sell
+            stop_price=str(ask_price + 10), # Price to trigger at (USD)
+            limit_price=str(ask_price), # Don't sell anything cheaper than this (USD) (Lower than stop price)
+            stop_direction='STOP_DIRECTION_STOP_UP', # Trigger when above stop_price
+        )
+    except Exception as e:
+        print(e)
+        order_info = {'success': False}
 
     usd_price = floor(btc_quantity * ask_price, 2)
 
+    order_id = None
     print("[{}] [SELL] ".format(get_now_string()), end='')
     if not 'success' in order_info or not order_info['success']:
-        print("[ERROR] ", end='')
+        print("[ERROR] [{}] ".format(order_info['error_response']['error']), end='')
+    else:
+        order_id = order_info['order_id']
 
     print("{:.5f} bitcoin for ${:.2f} (${:.2f} BTC).".format(btc_quantity, usd_price, ask_price))
+
+    return order_id
+
+def is_order_complete(ctx, order_id):
+    # No order
+    if not order_id:
+        return True
+
+    try:
+        # Get order
+        data = orders.get_order(ctx, order_id)
+        # Is complete for any state but OPEN
+        return data['order']['status'] != 'OPEN'
+    except Exception as e:
+        print(e)
+        print("[{}] [CHECK] [ERROR] Failed to get order {} status".format(get_now_string(), order_id))
+        # Failed: Assume still open
+        pass
+
+    return False
+
+def cancel_order(ctx, cancel_type, order_id):
+    try:
+        finish = orders.cancel_orders(
+            ctx,
+            order_ids=[order_id],
+        )
+        success = finish['results'][0]['success']
+        print("[{}] [CANCEL: {}] {}".format(get_now_string(), cancel_type, "Success" if success else "Fail"))
+        return True
+    except Exception as e:
+        print(e)
+        print("[{}] [CANCEL: {}] [ERROR]".format(get_now_string(), cancel_type))
+        pass
+
+    return False
