bf6a65f8fcf29f2121302849f8a020a23401a27d
GhwFluffy <ghw@ghwfluffy.com>
2025-02-01 08:48:57 -0600

    Making watchdog script end gracefully

--- cpp/watchdog.sh
+++ cpp/watchdog.sh
@@ -3,34 +3,68 @@
 # Run the tradebot in a loop and create a massif memory profile
 # when memory allocation starts running away
 #
+# memparse and allmemparse sources can be found in GhwFluffy's .ghw repository
+#
 
-HIGH_HEAP_MEMORY=125040
-HIGH_ALL_MEMORY=2529000
+set -eu -o pipefail
+
+HIGH_HEAP_MEMORY=225000 # 225MB
+HIGH_ALL_MEMORY=2529000 # 2.5GB
+
+BOTPID=0
+WATCHPID=0
+
+cleanup() {
+    kill -TERM "${WATCHPID}" 2>/dev/null || true
+    kill -TERM "${BOTPID}" 2>/dev/null || true
+    exit 0
+}
+trap cleanup SIGINT SIGTERM
 
 watchMemory() {
     while true; do
         sleep 1
-        PID=($(ps -ef | grep tradebot | grep -v 'grep\|cc1plus\|c++' | awk '{print $2}'))
+        PID=($(ps -ef | grep tradebot | awk '{print $2}'))
         for pid in "${PID[@]}"; do
-            MEMORY="$(memparse "${pid}" | awk '{print $4}')"
-            if [ ! -z "${MEMORY}" ] && [ ${MEMORY} -ge ${HIGH_HEAP_MEMORY} ]; then
-                kill -s USR2 "${pid}"
-                sleep 20
+            if [ ! -f "/proc/${pid}/cmdline" ]; then
+                continue
+            fi
+            # Find command being run
+            CMD="$(cat "/proc/${pid}/cmdline" | sed 's;\x00;\n;g' | head -n1 || true)"
+            if [ ! -z "${CMD}" ]; then
+                CMD="$(basename "${CMD}")"
+            fi
+            # Only tradebot command
+            if [ "${CMD}" != "valgrind.bin" ] && [ "${CMD}" != "tradebot" ]; then
+                continue
+            fi
+            # Still alive?
+            if ! kill -s 0 "${pid}" &> /dev/null; then
                 continue
             fi
 
-            MEMORY="$(/home/tfuller/git/ghw/src/memparse/allmemparse "${pid}" | awk '{print $3}')"
-            if [ ! -z "${MEMORY}" ] && [ ${MEMORY} -ge ${HIGH_ALL_MEMORY} ]; then
-                kill -s USR2 "${pid}"
-                sleep 20
+            # Check heap memory
+            MEMORY="$(memparse "${pid}" | awk '{print $4}' || true)"
+            if [ -n "${MEMORY}" ] && [ "${MEMORY}" -ge "${HIGH_HEAP_MEMORY}" ]; then
+                kill -s USR2 "${pid}" || true
+                continue
+            fi
+
+            # Check all memory (valgrind)
+            MEMORY="$(allmemparse "${pid}" | awk '{print $3}' || true)"
+            if [ -n "${MEMORY}" ] && [ "${MEMORY}" -ge "${HIGH_ALL_MEMORY}" ]; then
+                kill -s USR2 "${pid}" || true
                 continue
             fi
         done
     done
 }
 
+# Start memory watcher in the background
 watchMemory &
+WATCHPID=$!
 
+# Run tradebot under massif
 VALGRIND=(
     valgrind
     --tool=massif
@@ -41,6 +75,6 @@ VALGRIND=(
 
 while true; do
     "${VALGRIND[@]}" ./build/tradebot &
-    PID=$!
-    wait ${PID}
+    BOTPID=$!
+    wait "${BOTPID}"
 done
